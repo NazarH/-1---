e87cc34d117966f23bb115e04e990eca90b67354
NazarH <soundcfavorite@gmail.com>
2021-08-06 16:43:51 +0300

    first

---
+++ js/script.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+    $('.header__btn').click(function(event){
+        $('.header__btn, .header__nav, .header__row, .header__item, .header__btn3, .header__logo, .header').toggleClass('active');
+    });
+})
